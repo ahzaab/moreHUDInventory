d704279a398c17ac472cbf73be44fcbfb47c86d7
ahzaab <ahzaab@outlook.com>
2022-09-22 17:30:29 -0400

    Updated to 1.6.640

--- src/main.cpp
+++ src/main.cpp
@@ -41,6 +41,7 @@ extern "C"
         v.AuthorName("Ahzaab"sv);
         v.CompatibleVersions({ SKSE::RUNTIME_LATEST });
         v.UsesAddressLibrary(true);
+        v.UsesStructsPost629(true);
         return v;
     }();
 
